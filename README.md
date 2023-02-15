ab4f98a1e32feb50d7867e99d28db562fb518399
Thomas Meyer <t.meyer@thatec-innovation.com>
2023-02-15 17:21:13 +0100

    First commit, to be tested

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Core_user_server_scripts
+
 Scripts to create the required files for thaTEC:Core to implement a user server to distribute own programs via thaTEC:Core
+
+## Requirements
+
+Requires Python 3 for the execution of the script.
+Tested with Python 3.10.5
+
+## Usage
+
+The user server / user directory must be a folder which contains a folder for each software module which should be distributed and updated using thaTEC:Core. The module name is given by the folder name and must not be identical to any module name available in the THATec Innovation device library!
+Furthermore, the module folder must contain exactly 1 .exe file.
+
+Place the *generate_Update.py* into the main folder and run the script. The script will search all folders for new and updated files and create additional required files within the module folders as well as a *Updates.csv* file in the main folder. Any found updates or deletions are printed to the terminal during the execution.
+
+Subsequently, set the path to the user directory in thaTEC:Core and you are good to go and share your own software modules within thaTEC:Core.
+
+In case of any issues, set the *verbose* option to *True* and check the output of the script.
+
+## Contact
+THATec Innovation GmbH
+https://thatec-innovation.com/
+contact@thatec-innovation.com
---
+++ generate_Update.py
@@ -0,0 +1,197 @@
+#!/usr/bin/python
+import csv
+import os
+from datetime import datetime
+import sys
+import hashlib
+
+verbose = False
+
+class upd_info:
+    Device = ""
+    Compiled = "0"
+
+    def get_attributes(self):
+        attribs = []
+        for attribute in upd_info.__dict__.keys():
+            if attribute[:2] != '__':
+                value = getattr(upd_info, attribute)
+                if not callable(value):
+                    attribs.append(attribute)
+        return(attribs)
+
+def get_existing_filelist(folder):
+    if os.path.exists('./'+folder+'/filelist.csv'):
+        try:
+            with open('./'+folder+'/filelist.csv', 'r') as csv_file: 
+                reader=csv.reader(csv_file, delimiter=";")
+                return(list(filter(None,reader))) # remove empty elements
+        except FileNotFoundError:
+            return None
+        except Exception as err:
+            print("ERROR reading old filelist in module {}: {}".format(folder,err))
+            sys.exit("Stopping execution")
+
+def get_files_in_folder(folder): # Check all existing files in folder and create MD5 sums
+    filelist_array=[] # array into which the filepaths and MD5 sums will be written
+    for dirname, dirnames, filenames in os.walk(folder):
+        for file in filenames:
+            if file == "filelist.csv" or str(file).endswith('.upd'): # skip filelist.csv and upd files
+                pass
+            else: #create MD5 sum
+                hash_md5 = hashlib.md5()
+                file_path = dirname + '\\' + file
+                try:
+                    with open(file_path, "rb") as file:
+                        for chunk in iter(lambda: file.read(4096), b""):
+                            hash_md5.update(chunk)
+                    file_path = str.replace(file_path,folder+"\\","",1) # remove folder from path
+                    filelist_array.append([file_path,'',hash_md5.hexdigest()]) # add path and hashsum into array
+                except Exception as err:
+                    print("ERROR reading MD5 sum from file {}: {}".format(file_path,err))
+                    sys.exit("Stopping execution")
+    return filelist_array
+
+def get_deleted_filelist_and_updates (old_filelist, new_filelist):
+    return_value = {
+        "deleted_filelist" : [],
+        "update" : False
+    }
+    for old_entry in old_filelist: # walk through old entries
+        if old_entry not in new_filelist: # check for differences (different hashsums) 
+            return_value["update"] = True
+        if old_entry[0] not in [row_new[0] for row_new in new_filelist] and not str(old_entry[0]).endswith('.upd') and old_entry[0] != "filelist.csv": 
+            #filename not in new_filelist => deletion (skip filelist.csv and upd files)
+            return_value["deleted_filelist"].append([old_entry[0],"delete",""])
+    return return_value
+
+def write_filelist_csv(folder, filelist):
+    try:
+        with open(folder+'/filelist.csv', 'w',newline='') as csv_file:
+            writer = csv.writer(csv_file, delimiter=';')
+            writer.writerows(filelist)
+    except Exception as err:
+        print("ERROR writing csv file in folder {}: {}".format(folder,err))
+        sys.exit("Stopping execution")
+
+def update_upd(folder):
+    info = read_write_upd(folder = folder, info = upd_info(), read = True)
+    now = datetime.now()
+    new_compiled_date = int(now.strftime("%Y%m%d00"))
+    if info.Device is not None:
+        if new_compiled_date <= int(info.Compiled): # possible if multiple updates are published in one day (up to 99 updates possible)
+            for i in range (100):
+                new_compiled_date += 1
+                if new_compiled_date > int(info.Compiled):
+                    break
+    else: #Device is None => there was no upd file so far => new module, create upd file
+        info.Device = folder
+    info.Compiled = str(new_compiled_date)
+    read_write_upd(folder = folder, info = info, read = False) # update upd file
+
+def read_write_upd(folder, info: upd_info, read: bool):
+    path = folder + "\\" + folder + ".upd"
+    if read:
+        info = upd_info()
+        try:
+            with open(path, "r") as upd_file:
+                for line in upd_file.readlines(): # walk through lines in upd file
+                    for attrib in info.get_attributes():
+                        if line.startswith(attrib):
+                            setattr(info, attrib,line.split(': ')[1].splitlines()[0]) # remove linebreaks
+            info.Device = folder # overwrite device name by folder name to make sure both match
+            return info
+        except FileNotFoundError:
+            info.Device = None
+            return info
+        except Exception as err:
+            print("ERROR reading .upd file in folder {}: {}".format(folder,err))
+            sys.exit("Stopping execution")
+    
+    else: # write upd file
+        try:
+            with open(path, "w") as upd_file:
+                for attrib in info.get_attributes():
+                    upd_file.write(attrib + ': ' + getattr(info, attrib) + "\n")
+        except Exception as err:
+            print("ERROR writing .upd file in folder {}: {}".format(folder,err))
+            sys.exit("Stopping execution")
+        return
+
+def write_Updates_csv(folder_list):
+    update_array = []
+    for folder in folder_list: # walk through all folders
+        info = read_write_upd(folder = folder, info = upd_info(), read = True)
+        if info.Device is not None:
+            update_array.append(info.Device + ';' + info.Compiled + ';')
+        else:
+            print("Upd file not found in folder {} to write into Updates.csv".format(folder))
+            sys.exit("Stopping execution")
+    try:
+        with open("Updates.csv","w",encoding="utf-8") as updates_file:
+            for entry in update_array:
+                updates_file.write(entry)
+                updates_file.write("\r")
+    except Exception as err:
+        print("ERROR writing Updates.csv in main folder: {}".format(err))
+        sys.exit("Stopping execution")
+
+def check_exe(filelist_array):
+    execs = 0
+    for file in [x[0] for x in filelist_array]:
+        if str(file).endswith('.exe'):
+            execs += 1
+    return execs == 1
+
+def main():
+    print("Python version: ")
+    print(sys.version)
+
+    all_module_folders = os.listdir('.')
+    all_module_folders.sort()
+    folder_list = []
+
+    for folder in all_module_folders: # walk through all folders
+        if os.path.isdir(folder) and not str(folder).startswith('.'):
+            update = False
+            if verbose: print("Current folder: {}".format(folder))
+            new_filelist = get_files_in_folder(folder)
+            if verbose: print("New filelist created")
+            if check_exe(new_filelist):
+                folder_list.append(folder)
+                old_filelist = get_existing_filelist(folder)
+                if old_filelist is not None:
+                    if verbose: print("Old filelist found, checking for deleted files")
+                    deletions = get_deleted_filelist_and_updates(old_filelist, new_filelist)
+                    if len(deletions["deleted_filelist"]) > 0 or deletions["update"]: # Deleted or updated files detected
+                        print("Deleted or updated files detected in " + folder)
+                        update = True
+                        print(new_filelist)
+                        print(deletions["deleted_filelist"])
+                        new_filelist += deletions["deleted_filelist"] #removed items will be included into the filelist with a "delete" flag in column 1 and will be removed when updating via thaTEC:Core
+                    else:
+                        if verbose: print("No deleted files found")
+                else: # no existing filelist found, check if upd file is available
+                    info = read_write_upd(folder = folder, info = upd_info(), read = True)
+                    if info.Device is None: # upd does not exist
+                        update = True
+                        print("New module found: {}".format(folder))
+                    else:
+                        print("No filelist but upd file found in folder {}.")
+                        x = input("Continue and create new filelist? (y/n):")
+                        if x != 'y':
+                            sys.exit('Stopping execution')
+                        else:
+                            update = True
+                if update: # write filelist and upd file
+                    print("Updating filelist.csv")
+                    write_filelist_csv(folder, new_filelist) #write new filelist to folder
+                    print("Updating upd file")
+                    update_upd(folder) # update upd file with new compiled date
+            else:
+                print("Error for folder {}. Number of executables (.exe) is not 1! Folder skipped.".format(folder))
+    if verbose: print("=== Checking folders finished, writing Updates.csv")
+    write_Updates_csv(folder_list) # after writing all required files in module folders => write (global) Updates.csv containing the information on all modules
+
+if __name__ == "__main__":
+    main()
